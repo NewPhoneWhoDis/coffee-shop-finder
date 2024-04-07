ea4d4efa005234f3fb553d4d21cd841ddcb9470a
Kristiyan Merdzhov <88944722+NewPhoneWhoDis@users.noreply.github.com>
2024-04-07 13:36:23 +0200

    basic design concept

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import Banner from "@/components/custom/banner.client";
+import Link from "next/link";
 
 export default function Home() {
+  const coffeeStoreId = "test-coffee-store";
+  const cityName = "Berlin";
   return (
-    <main className="flex min-h-screen flex-col items-start justify-between p-14">
-      <Banner></Banner>
-    </main>
+    <div className="mb-56">
+      <main className="mx-auto mt-10 max-w-6xl flex flex-col items-start p-14">
+        <Banner></Banner>
+        <Link className="mt-4" href={`/coffee-store/${coffeeStoreId}`}>
+          Test coffee shop
+        </Link>
+      </main>
+
+      <p className="text-xl ml-4 font-bold"> Coffee Shops in {cityName}</p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="ml-4">test</div>
+        <div>test 2</div>
+        <div>test 3</div>
+      </div>
+    </div>
   );
 }
