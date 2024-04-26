a8152dd53f4508c3e0d382787352df9967e40e27
Kristiyan Merdzhov <88944722+NewPhoneWhoDis@users.noreply.github.com>
2024-04-27 00:40:17 +0200

    Added card styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Banner from "@/components/custom/banner.client";
 import Link from "next/link";
-import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
 
 export default function Home() {
   const coffeeStoreId = "test-coffee-store";
@@ -16,10 +23,32 @@ export default function Home() {
 
       <p className="text-xl ml-4 font-bold"> Coffee Shops in {cityName}</p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
         {Array.from({ length: 5 }).map((_, index) => (
-          <Card key={index}>
-            <CardContent>Test street: 322 Adress: 233 City: 313</CardContent>
+          <Card
+            key={index}
+            className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+          >
+            <CardHeader>
+              <Image
+                className="rounded-t-lg"
+                src="https://via.placeholder.com/400"
+                alt="Coffee Shop"
+                width={40}
+                height={50}
+              />
+            </CardHeader>
+            <CardContent>
+              <CardTitle>Coffee Paradise</CardTitle>
+              <CardDescription>
+                A cozy place to enjoy your day and sip on the best artisan
+                coffee in town.
+              </CardDescription>
+              <p className="text-sm text-gray-500 mt-1">
+                123 Coffee Lane, Beanstown, CA
+              </p>
+              <p className="text-sm text-gray-500">Open: 08:00 - 20:00</p>
+            </CardContent>
           </Card>
         ))}
       </div>
